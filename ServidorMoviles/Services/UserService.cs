177524d99b2fef3558c0cd0f482057332c3b86fc
Pablo Menéndez Barrientos <UO237542@uniovi.es>
2018-04-14 23:18:42 +0200

    Add project files.

---
+++ ServidorMoviles/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServidorMoviles.Services
+{
+    public class UserService : IUserService
+    {
+
+    }
+}
